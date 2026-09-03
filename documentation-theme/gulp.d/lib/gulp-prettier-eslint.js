eeda2d343fe558e57d8160ab26b8e10ff5281af4
scphantm <scphantm@gmail.com>
2026-09-03 12:25:54 -0400

    Activated prettier on the rest of the project, everything is now cleaned up.

--- documentation-theme/gulp.d/lib/gulp-prettier-eslint.js
+++ documentation-theme/gulp.d/lib/gulp-prettier-eslint.js
@@ -1,44 +1,49 @@
-'use strict'
+"use strict";
 
-const log = require('fancy-log')
-const PluginError = require('plugin-error')
-const prettierEslint = require('prettier-eslint')
-const { Transform } = require('stream')
-const map = (transform) => new Transform({ objectMode: true, transform })
+const log = require("fancy-log");
+const PluginError = require("plugin-error");
+const prettierEslint = require("prettier-eslint");
+const { Transform } = require("stream");
+const map = (transform) => new Transform({ objectMode: true, transform });
 
 module.exports = () => {
-  const report = { changed: 0, unchanged: 0 }
-  return map(format).on('finish', () => {
+  const report = { changed: 0, unchanged: 0 };
+  return map(format).on("finish", () => {
     if (report.changed > 0) {
-      const changed = 'formatted '
+      const changed = "formatted "
         .concat(report.changed)
-        .concat(' file')
-        .concat(report.changed === 1 ? '' : 's')
-      const unchanged = 'left '
+        .concat(" file")
+        .concat(report.changed === 1 ? "" : "s");
+      const unchanged = "left "
         .concat(report.unchanged)
-        .concat(' file')
-        .concat(report.unchanged === 1 ? '' : 's')
-        .concat(' unchanged')
-      log(`prettier-eslint: ${changed}; ${unchanged}`)
+        .concat(" file")
+        .concat(report.unchanged === 1 ? "" : "s")
+        .concat(" unchanged");
+      log(`prettier-eslint: ${changed}; ${unchanged}`);
     } else {
-      log(`prettier-eslint: left ${report.unchanged} file${report.unchanged === 1 ? '' : 's'} unchanged`)
+      log(
+        `prettier-eslint: left ${report.unchanged} file${report.unchanged === 1 ? "" : "s"} unchanged`,
+      );
     }
-  })
+  });
 
-  function format (file, enc, next) {
-    if (file.isNull()) return next()
-    if (file.isStream()) return next(new PluginError('gulp-prettier-eslint', 'Streaming not supported'))
+  function format(file, enc, next) {
+    if (file.isNull()) return next();
+    if (file.isStream())
+      return next(
+        new PluginError("gulp-prettier-eslint", "Streaming not supported"),
+      );
 
-    const input = file.contents.toString()
-    const output = prettierEslint({ text: input, filePath: file.path })
+    const input = file.contents.toString();
+    const output = prettierEslint({ text: input, filePath: file.path });
 
     if (input === output) {
-      report.unchanged += 1
+      report.unchanged += 1;
     } else {
-      report.changed += 1
-      file.contents = Buffer.from(output)
+      report.changed += 1;
+      file.contents = Buffer.from(output);
     }
 
-    next(null, file)
+    next(null, file);
   }
-}
+};
--- documentation-theme/gulp.d/tasks/index.js
+++ documentation-theme/gulp.d/tasks/index.js
@@ -1,5 +1,9 @@
-'use strict'
+"use strict";
 
-const camelCase = (name) => name.replace(/[-]./g, (m) => m.slice(1).toUpperCase())
+const camelCase = (name) =>
+  name.replace(/[-]./g, (m) => m.slice(1).toUpperCase());
 
-module.exports = require('require-directory')(module, __dirname, { recurse: false, rename: camelCase })
+module.exports = require("require-directory")(module, __dirname, {
+  recurse: false,
+  rename: camelCase,
+});
